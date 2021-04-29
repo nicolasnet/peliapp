5ae93b39df25971b8fe02927a571fe0aa747e882
Nicolas <you@example.com>
2021-04-28 22:59:57 -0300

    cambio favicon y modificacion clase Actor

--- src/app/clases/actor.ts
+++ src/app/clases/actor.ts
@@ -2,9 +2,8 @@ export class Actor {
     nombre: string;
     apellido: string;
     email: string;
-    pais: [
-        nombre: string,
-        imagen: string
-    ];
+    pais: object;
+    direccion: string;
+    
 
 }
